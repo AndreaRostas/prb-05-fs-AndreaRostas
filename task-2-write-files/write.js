d369dcde6dfb92bba677bee6fdd86549ecb2a6fa
Andrea Rostas <andrea.rostas58@gmail.com>
2025-04-10 16:03:50 +0200

    finished

--- task-2-write-files/write.js
+++ task-2-write-files/write.js
@@ -1,29 +1,21 @@
 import fs from "fs";
 
 function updateFile(filename) {
-  try{
+
      // Lies die Datei und speichere den Inhalt als String
-     const data = fs.readFileSync(filename, 'utf8');
-    
+     const data = fs.readFileSync(filename, 'utf8')
+   
      // Füge ein '*' zum Inhalt hinzu
-     const updatedData = data + '*';
+     const updatedData = `${data} *`;
  
      // Schreibe den geänderten Inhalt zurück in die Datei
      fs.writeFileSync(filename, updatedData, 'utf8');
-     console.log(`Datei ${filename} erfolgreich aktualisiert.`);
-  }catch (error){
-    // Wenn ein Fehler auftritt (z. B. Datei nicht gefunden), stoppt das Programm und gibt eine Fehlermeldung aus
-    console.error(`Fehler beim Bearbeiten der Datei ${filename}:`, error.message);
-    process.exit(1); // Beendet das Programm mit einem Fehlercode
+     
   }
-}
+
 
 updateFile('review.txt');
-updateFile('review.txt');
-updateFile('review.txt');
-updateFile('review.txt');
-
 
 // Versuche, updateFile mit der Datei 'star.txt' aufzurufen (dies sollte zum Absturz führen)
-updateFile('star.txt');
+//  updateFile('star.txt');
 
